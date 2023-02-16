c66004b2b1d825e8c56d7c396afb177c5cca6242
Linkan Kumar Sahu <sahulinkan7@gmail.com>
2023-02-16 15:34:10 +0530

    added custom exception code

---
+++ finance_complaint/exception.py
@@ -0,0 +1,27 @@
+import os,sys
+
+class FinanceException(Exception):
+    def __init__(self,error_message:Exception,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=FinanceException.get_detailed_error_message(error_message=error_message,
+                                                                        error_detail=error_detail)
+
+    @staticmethod
+    def get_detailed_error_message(error_message:Exception,error_detail:sys)->str:
+        """
+        error_message: Exception object
+        error_detail: object of sys module
+        """
+        _, _,exec_tab=error_detail.exc_info()
+        try_block_line_number=exec_tab.tb_lineno
+        file_name=exec_tab.tb_frame.f_code.co_filename
+        error_message=f"""
+        Error occured in script: [{file_name}] at line number [{try_block_line_number}]
+        message : [{error_message}]
+        """
+        return error_message
+    def __str__(self):
+        return self.error_message
+
+    def __repr__(self):
+        return str(FinanceException.__name__)
